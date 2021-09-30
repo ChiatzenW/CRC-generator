38e13bbf75c124ea6d104d8291e883b06f767374
ChiatzenW <chiatzen.wang@mail.utronto.ca>
2021-09-30 13:01:53 -0400

    add_image

--- CRC-generator.py
+++ CRC-generator.py
@@ -1,17 +1,29 @@
 from PIL import Image
+from os import path
 
-def new_crc_menu():
-    opt = 2
-    print("What do you want to do now?")
-    print("1. Create new CRC card.")
-    print("2. Exit.")
+class CRC_generator():
+    def main():
+        if path.isfile('crc.jpg'):
+            img = Image.open(crc.jpg)
+        else:
+            img = Image.new('RGB', (720, 576), (225,225,225))
+            img.save("crc.jpg")
 
-    return int(input())
-def new_nre_class():
-    class_name = ""
-    class_name = input("Class name:")
+    def new_crc_menu():
+        opt = 2
+        print("What do you want to do now?")
+        print("1. Create new CRC card.")
+        print("2. Exit.")
 
-def new_sub_line():
-    class_name = ""
-    class_name = input("Class name:")
+        return int(input())
+
+    def new_class():
+        class_name = ""
+        class_name = input("Class name:")
+
+    def new_sub_line():
+        res = ""
+        collab=""
+        res = input("Responsibility:")
+        collab = input("Collaborator:")
 
